060ab29f897edea22b27a2cb0a813807c6468562
lizuyao2010 <zuyaoli2010@gmail.com>
2016-09-01 16:58:45 -0700

    first commit

---
+++ data_utils.py
@@ -0,0 +1,141 @@
+from __future__ import absolute_import
+
+import os
+import re
+import numpy as np
+
+def load_candidates(data_dir, task_id):
+    assert task_id > 0 and task_id < 21
+    candidates=[]
+    with open(os.path.join(data_dir,'dialog-babi-candidates.txt')) as f:
+        for line in f:
+            line=line.lower().strip().split(' ')[1:]
+            if line[0]=='api_call':
+                candidates.append(line)
+    return candidates,dict((' '.join(cand),i) for i,cand in enumerate(candidates))
+
+
+def load_dialog_task(data_dir, task_id, candid_dic):
+    '''Load the nth task. There are 20 tasks in total.
+
+    Returns a tuple containing the training and testing data for the task.
+    '''
+    assert task_id > 0 and task_id < 7
+
+    files = os.listdir(data_dir)
+    files = [os.path.join(data_dir, f) for f in files]
+    s = 'dialog-babi-task{}-'.format(task_id)
+    train_file = [f for f in files if s in f and 'trn' in f][0]
+    test_file = [f for f in files if s in f and 'tst.' in f][0]
+    val_file = [f for f in files if s in f and 'dev' in f][0]
+    train_data = get_dialogs(train_file,candid_dic)
+    test_data = get_dialogs(test_file,candid_dic)
+    val_data = get_dialogs(val_file,candid_dic)
+    return train_data, test_data, val_data
+
+
+def tokenize(sent):
+    '''Return the tokens of a sentence including punctuation.
+    >>> tokenize('Bob dropped the apple. Where is the apple?')
+    ['Bob', 'dropped', 'the', 'apple', '.', 'Where', 'is', 'the', 'apple', '?']
+    '''
+    if sent=='<silence>':
+        return [sent]
+    return [x.strip() for x in re.split('(\W+)?', sent) if x.strip()]
+
+
+def parse_dialogs(lines,candid_dic):
+    '''
+        Parse dialogs provided in the babi tasks format
+    '''
+    data=[]
+    context=[]
+    u=None
+    r=None
+    for line in lines:
+        line=str.lower(line.strip())
+        if line:
+            nid, line = line.split(' ', 1)
+            nid = int(nid)
+            u, r = line.split('\t')
+            u = tokenize(u)
+            if u[-1] == '.' or u[-1] == '?':
+                u = u[:-1]
+            r = tokenize(r)
+            if r[-1] == '.' or r[-1] == '?':
+                r = r[:-1]
+            # temporal encoding, and utterance/response encoding
+            u.append('$u')
+            u.append('#'+str(nid))
+            r.append('$r')
+            r.append('#'+str(nid))
+            context.append(u)
+            context.append(r)
+        else:
+            context=[x for x in context[:-2] if x]
+            u=u[:-2]
+            r=r[:-2]
+            key=' '.join(r)
+            if key in candid_dic:
+                r=candid_dic[key]
+                data.append((context, u,  r))
+            context=[]
+    return data
+
+
+
+def get_dialogs(f,candid_dic):
+    '''Given a file name, read the file, retrieve the dialogs, and then convert the sentences into a single dialog.
+    If max_length is supplied, any stories longer than max_length tokens will be discarded.
+    '''
+    with open(f) as f:
+        return parse_dialogs(f.readlines(),candid_dic)
+
+def vectorize_candidates(candidates,word_idx):
+    # result=[]
+    vec=np.zeros((len(candidates),len(word_idx)+1))
+    for i,candidate in enumerate(candidates):
+        for w in candidate:
+            vec[i][word_idx[w]]=1
+        # result.append([word_idx[w] for w in candidate])
+    # return np.array(result)
+    return vec
+
+def vectorize_data(data, word_idx, sentence_size, memory_size, candidates_size):
+    """
+    Vectorize stories and queries.
+
+    If a sentence length < sentence_size, the sentence will be padded with 0's.
+
+    If a story length < memory_size, the story will be padded with empty memories.
+    Empty memories are 1-D arrays of length sentence_size filled with 0's.
+
+    The answer array is returned as a one-hot encoding.
+    """
+    S = []
+    Q = []
+    A = []
+    for story, query, answer in data:
+        ss = []
+        for i, sentence in enumerate(story, 1):
+            ls = max(0, sentence_size - len(sentence))
+            ss.append([word_idx[w] for w in sentence] + [0] * ls)
+
+        # take only the most recent sentences that fit in memory
+        ss = ss[::-1][:memory_size][::-1]
+
+        # pad to memory_size
+        lm = max(0, memory_size - len(ss))
+        for _ in range(lm):
+            ss.append([0] * sentence_size)
+
+        lq = max(0, sentence_size - len(query))
+        q = [word_idx[w] for w in query] + [0] * lq
+
+        y = np.zeros(candidates_size)
+        y[answer]=1
+
+        S.append(ss)
+        Q.append(q)
+        A.append(y)
+    return np.array(S), np.array(Q), np.array(A)
---
+++ single_dialog.py
@@ -0,0 +1,122 @@
+from __future__ import absolute_import
+from __future__ import print_function
+
+from data_utils import load_dialog_task, vectorize_data, load_candidates, vectorize_candidates
+from sklearn import metrics
+from memn2n import MemN2NDialog
+from itertools import chain
+from six.moves import range, reduce
+
+import tensorflow as tf
+import numpy as np
+
+tf.flags.DEFINE_float("learning_rate", 0.01, "Learning rate for Adam Optimizer.")
+tf.flags.DEFINE_float("epsilon", 1e-8, "Epsilon value for Adam Optimizer.")
+tf.flags.DEFINE_float("max_grad_norm", 40.0, "Clip gradients to this norm.")
+tf.flags.DEFINE_integer("evaluation_interval", 10, "Evaluate and print results every x epochs")
+tf.flags.DEFINE_integer("batch_size", 32, "Batch size for training.")
+tf.flags.DEFINE_integer("hops", 3, "Number of hops in the Memory Network.")
+tf.flags.DEFINE_integer("epochs", 200, "Number of epochs to train for.")
+tf.flags.DEFINE_integer("embedding_size", 20, "Embedding size for embedding matrices.")
+tf.flags.DEFINE_integer("memory_size", 50, "Maximum size of memory.")
+tf.flags.DEFINE_integer("task_id", 1, "bAbI task id, 1 <= id <= 20")
+tf.flags.DEFINE_integer("random_state", None, "Random state.")
+tf.flags.DEFINE_string("data_dir", "data/dialog-bAbI-tasks/", "Directory containing bAbI tasks")
+FLAGS = tf.flags.FLAGS
+
+print("Started Task:", FLAGS.task_id)
+
+
+candidates,candid_dic = load_candidates(FLAGS.data_dir, FLAGS.task_id)
+# task data
+train, test, val = load_dialog_task(FLAGS.data_dir, FLAGS.task_id, candid_dic)
+data = train + test + val
+
+vocab = reduce(lambda x, y: x | y, (set(list(chain.from_iterable(s)) + q) for s, q, a in data))
+vocab |= reduce(lambda x,y: x|y, (set(candidate) for candidate in candidates) )
+vocab=sorted(vocab)
+word_idx = dict((c, i + 1) for i, c in enumerate(vocab))
+
+max_story_size = max(map(len, (s for s, _, _ in data)))
+mean_story_size = int(np.mean([ len(s) for s, _, _ in data ]))
+sentence_size = max(map(len, chain.from_iterable(s for s, _, _ in data)))
+query_size = max(map(len, (q for _, q, _ in data)))
+memory_size = min(FLAGS.memory_size, max_story_size)
+vocab_size = len(word_idx) + 1 # +1 for nil word
+sentence_size = max(query_size, sentence_size) # for the position
+
+print ("vocab size:",vocab_size)
+print("Longest sentence length", sentence_size)
+print("Longest story length", max_story_size)
+print("Average story length", mean_story_size)
+
+# train/validation/test sets
+candidates_vec=vectorize_candidates(candidates,word_idx)
+n_cand = candidates_vec.shape[0]
+print("Candidate Size", n_cand)
+
+trainS, trainQ, trainA = vectorize_data(train, word_idx, sentence_size, memory_size, n_cand)
+testS, testQ, testA = vectorize_data(test, word_idx, sentence_size, memory_size, n_cand)
+valS, valQ, valA = vectorize_data(val, word_idx, sentence_size, memory_size, n_cand)
+
+
+print(testS[0])
+
+print("Training set shape", trainS.shape)
+
+# params
+n_train = trainS.shape[0]
+n_test = testS.shape[0]
+n_val = valS.shape[0]
+
+print("Training Size", n_train)
+print("Validation Size", n_val)
+print("Testing Size", n_test)
+
+train_labels = np.argmax(trainA, axis=1)
+test_labels = np.argmax(testA, axis=1)
+val_labels = np.argmax(valA, axis=1)
+
+tf.set_random_seed(FLAGS.random_state)
+batch_size = FLAGS.batch_size
+optimizer = tf.train.AdamOptimizer(learning_rate=FLAGS.learning_rate, epsilon=FLAGS.epsilon)
+
+batches = zip(range(0, n_train-batch_size, batch_size), range(batch_size, n_train, batch_size))
+batches = [(start, end) for start, end in batches]
+
+with tf.Session() as sess:
+    model = MemN2NDialog(batch_size, vocab_size, n_cand,sentence_size, memory_size, FLAGS.embedding_size, candidates_vec, session=sess,
+                   hops=FLAGS.hops, max_grad_norm=FLAGS.max_grad_norm, optimizer=optimizer)
+    for t in range(1, FLAGS.epochs+1):
+        np.random.shuffle(batches)
+        total_cost = 0.0
+        for start, end in batches:
+            s = trainS[start:end]
+            q = trainQ[start:end]
+            a = trainA[start:end]
+            cost_t = model.batch_fit(s, q, a)
+            total_cost += cost_t
+
+        if t % FLAGS.evaluation_interval == 0:
+            train_preds = []
+            for start in range(0, n_train, batch_size):
+                end = start + batch_size
+                s = trainS[start:end]
+                q = trainQ[start:end]
+                pred = model.predict(s, q)
+                train_preds += list(pred)
+
+            val_preds = model.predict(valS, valQ)
+            train_acc = metrics.accuracy_score(np.array(train_preds), train_labels)
+            val_acc = metrics.accuracy_score(val_preds, val_labels)
+
+            print('-----------------------')
+            print('Epoch', t)
+            print('Total Cost:', total_cost)
+            print('Training Accuracy:', train_acc)
+            print('Validation Accuracy:', val_acc)
+            print('-----------------------')
+
+    test_preds = model.predict(testS, testQ)
+    test_acc = metrics.accuracy_score(test_preds, test_labels)
+    print("Testing Accuracy:", test_acc)
